b7379dcf83da3eab7783ae10ecccd7940edfac91
Manish Thulung <manish.thulung58@gmail.com>
2025-01-12 22:24:53 +0545

    ws set-up

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -4,6 +4,14 @@ import { JWT_SECRET } from "@repo/backend-common/config";
 
 const wss = new WebSocketServer({ port: 8009 });
 
+interface User {
+  ws: WebSocket;
+  rooms: string[];
+  userId: string;
+}
+
+const users: User[] = [];
+
 wss.on("connection", function connection(ws, req) {
   const url = req.url;
   if (!url) {
@@ -16,14 +24,49 @@ wss.on("connection", function connection(ws, req) {
   const decoded = jwt.verify(token, JWT_SECRET);
 
   // @ts-ignore
-  if (!decoded && !decoded.userId) {
+  if (!decoded && !decoded.payload.id) {
     ws.close();
     return;
   }
+
+  users.push({
+    ws,
+    rooms: [],
+    userId: decoded.payload.id,
+  });
+
   ws.on("error", console.error);
 
   ws.on("message", function message(data) {
-    console.log("received: %s", data);
+    const parsedData = JSON.parse(data as unknown as string);
+
+    if (parsedData.type == "join_room") {
+      const user = users.filter((user) => user.ws === ws);
+      user?.rooms?.push(parsedData.roomId);
+    }
+
+    if (parsedData.type == "leave_room") {
+      const user = users.filter((user) => user.ws === ws);
+      if (!user) {
+        return;
+      }
+      user?.rooms = user?.rooms?.filter((user) => user.ws === ws);
+    }
+    if (parsedData.type == "chat") {
+      const roomId = parsedData.roomId;
+      const message = parsedData.message;
+
+      users.forEach((user) => {
+        const part = user.rooms.includes(roomId);
+        part && user.ws.send(message);
+      });
+
+      const user = users.filter((user) => user.ws === ws);
+      if (!user) {
+        return;
+      }
+      user?.rooms = user?.rooms?.filter((user) => user.ws === ws);
+    }
   });
 
   ws.send("something");
